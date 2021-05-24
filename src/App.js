6252a4274e7ed03c2a950faea730948065fcd4b5
jonjonnyjonjon <jonathanwong99@gmail.com>
2021-05-24 14:50:03 +0800

    covered most use cases
    - left with styling the buttons and design

--- src/App.js
+++ src/App.js
@@ -14,48 +14,96 @@ class App extends React.Component {
         this.handleClick = this.handleClick.bind(this)
     }
 
+    evaluateExpression(num1, num2, operator) {
+        var total = 0
+        if (operator === "+") {
+            total = parseInt(num1) + parseInt(num2)
+        } else if (operator === "-") {
+            total = parseInt(num1) - parseInt(num2)
+        } else if (operator === "x") {
+            total = parseInt(num1) * parseInt(num2)
+        } else {
+            total = parseInt(num1) / parseInt(num2)
+        }
+
+        return total
+    }
+
     handleClick(value) {
         var operator = this.state.operator
 
-        // num1
-        if (operator === "" && !isNaN(value)) {
-            this.setState(prevState => {
-                return {
-                    num1: "" + prevState.num1 + value,
-                    toDisplay: "" + prevState.num1 + value
-                }
-            })
-
-        // num2
-        } else if (operator !== "" && !isNaN(value)) {
-            this.setState(prevState => {
-                console.log(this.state.toDisplay)
-                return {
-                    num2: "" + prevState.num2 + value,
-                    toDisplay: "" + prevState.num2 + value
-                }
-            })            
-
-        // value is "="
-        } else if (value === "=") {
-            var total = 0
-            if (operator !== "") {
+        // if value is a number
+        if (!isNaN(value)) {
+            if (operator === "=") {
                 this.setState(prevState => {
-                    if (operator === "+") {
-                        total = parseInt(prevState.num1) + parseInt(prevState.num2)
-                    } else if (operator === "-") {
-                        total = parseInt(prevState.num1) - parseInt(prevState.num2)
-                    } else if (operator === "x") {
-                        total = parseInt(prevState.num1) * parseInt(prevState.num2)
-                    } else {
-                        total = parseInt(prevState.num1) / parseInt(prevState.num2)
+                    return {
+                        num1: "" + prevState.num1 + value,
+                        toDisplay: "" + prevState.num1 + value
                     }
+                })
+
+            // num1
+            } else if (operator === "") {
+                this.setState(prevState => {
+                    return {
+                        num1: "" + prevState.num1 + value,
+                        toDisplay: "" + prevState.num1 + value
+                    }
+                })
+
+            // num2
+            } else if (operator !== "") {
+                this.setState(prevState => {
+                    return {
+                        num2: "" + prevState.num2 + value,
+                        toDisplay: "" + prevState.num2 + value
+                    }
+                })
+            }            
+
+        // value is an operator
+        } else if ("+-x/".includes(value)) {
+
+            // operator clicked after clicking "="
+            if (operator === "=") {
+                this.setState(prevState => {
+                    return {
+                        num1: prevState.toDisplay,
+                        operator: value
+                    }
+                })
+
+            // very first operator clicked
+            } else if (this.state.num2 === "") {
+                this.setState({
+                    operator: value
+                })
+                
+            // subsequent operators clicked, i.e. 1+1+1
+            } else {
+                this.setState(prevState => {
+                    var total = this.evaluateExpression(prevState.num1, prevState.num2, operator)
 
                     return {
                         num1: total,
                         num2: "",
                         toDisplay: total,
-                        operator: ""
+                        operator: value
+                    }
+                })
+            }
+
+        // value is "="
+        } else if (value === "=") {
+            if (this.state.num1 !== "" && this.state.num2 !== "") {
+                this.setState(prevState => {
+                    var total = this.evaluateExpression(prevState.num1, prevState.num2, operator)
+
+                    return {
+                        num1: "",
+                        num2: "",
+                        toDisplay: total,
+                        operator: "="
                     }
                 })
             }
@@ -69,25 +117,27 @@ class App extends React.Component {
                 toDisplay: ""
             })
 
-        // value is other operators
-        } else {
-            this.setState({
-                operator: value
-            })
         }
     }
 
     render() {
         const buttonStyle = {
+            backgroundColor: "grey",
             width: "20%"
         }
+
+        const displayBox = {
+            display: "block",
+            backgroundColor: "lightblue",
+        }
+
         return (
             <div>
-                <div>
-                    {this.state.toDisplay}
+                <div style={displayBox}>
+                    Current total: {this.state.toDisplay}
                     <br />
-                    Current operator: 
-                    {this.state.operator}
+                    Current operator: {this.state.operator}
+                    &nbsp;
                 </div>
 
                 <div>
@@ -112,9 +162,10 @@ class App extends React.Component {
                     <Button style={buttonStyle} value="0" handleClick={this.handleClick} />
                     <Button style={buttonStyle} value="/" handleClick={this.handleClick} />
                     <Button style={buttonStyle} value="=" handleClick={this.handleClick} />
-                    <br/>
-
+                    <br />
+                        
                     <Button style={buttonStyle} value="clear" handleClick={this.handleClick} />
+                            
 
                 </div>
             </div>
